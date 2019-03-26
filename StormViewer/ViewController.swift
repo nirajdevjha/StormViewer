c0fc3fca8a5237711cf2cbf16081b4a738293137
nirajdevjha <nirajdev.jha@gmail.com>
2019-03-26 15:07:29 +0530

    Getting images from Bundle.

--- StormViewer/ViewController.swift
+++ StormViewer/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let fileManager = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fileManager.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                // this is a picture to load..
+                pictures.append(item)
+            }
+        }
+        print(pictures)
     }
 
 
